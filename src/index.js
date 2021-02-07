f42176db0f4179f4ec8da1be8bc919011b079504
Denis Savitskiy <denissavitski@strim-tech.com>
2021-02-07 20:32:30 +0300

    feat: add check brackets function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const closeSymbols = [];
+  let currentPair;
+  for (const currentS of str) {
+    if ((currentPair = bracketsConfig.find(el => el[1] === currentS))) {
+      if (closeSymbols.length > 0 && closeSymbols[closeSymbols.length - 1] === currentS) {
+        closeSymbols.pop()
+      } else if (currentPair[0] === currentS) {
+        closeSymbols.push(currentPair[1]);
+      } else {
+        return false;
+      }
+    } else if ((currentPair = bracketsConfig.find(el => el[0] === currentS))) {
+      closeSymbols.push(currentPair[1]);
+    }
+  }
+  return closeSymbols.length === 0;
 }
